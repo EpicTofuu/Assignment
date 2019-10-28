6c097a6618ead548925bce2e3044bf2dbe7c9231
EpicTofuu <rinkaandchan@outlook.com>
2019-10-29 00:06:21 +1100

    implemented unicode encoding

--- Section 1/CaeserCipher.py
+++ Section 1/CaeserCipher.py
@@ -1,17 +1,4 @@
 import random
-
-# adds two hexadecimal numbers and returns them both in hex
-def AddHex (a, b):
-    value = int (a) + int (b)
-    return hex (value)
-
-# Finds the index of a given element in an array
-# XXX TODO, check if array.Find () is a thing and utilise if it is
-def FindInArray (array, element):
-    for i in range (len (array)):
-        if (array[i] == element):
-            return i
-
 # encrypts the given string "userIn" using a randomly generated key
 # userIn: the string to be encrypted
 def Caesar_Encrypt (userIn):
@@ -22,18 +9,7 @@ def Caesar_Encrypt (userIn):
 # userIn: the string to be encrypted.
 # key: the number of places to shift each character
 # returns: the encrypted string
-def Caesar_Encrypt_Key (userIn, key):
-
-    # TODO test other possibilities other than a string list
-
-    # Firstly, an alphabet array needs to be created
-    # TODO move this out of the main method
-    __alphabet__ = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" # TODO implement unicode standard/custom alphabet sets
-    __alphabetList__ = []
-    
-    for n in __alphabet__:
-        __alphabetList__.append (n)
-    
+def Caesar_Encrypt_Key (userIn, key):  
     InList = []     #list to store user input
 
     # Convert the string input to character list
@@ -41,7 +17,12 @@ def Caesar_Encrypt_Key (userIn, key):
         InList.append (i)
 
     # interate over each and every single character in the list
-    for a in range (len (InList)):        
+    for a in range (len (InList)):  
+        inIndex = ord (InList[a])                   # find the index of the letter in the *input*
+        InList[a]= chr((inIndex + key) % 137439)    # convert back to character    
+
+        # legacy implementation (Only capable of capital alphabet bAD)
+        '''      
         # find the index of the letter in the *input*
         inIndex = FindInArray (__alphabetList__, InList[a])
 
@@ -50,16 +31,15 @@ def Caesar_Encrypt_Key (userIn, key):
             raise Exception ("Character not supported")
 
         InList[a] = __alphabetList__[(inIndex + key) % len(__alphabetList__)]
+        '''
 
-    # conver the list back into a string
+    # convert the list back into a string
     value = ""
     for i in InList:
         value += i
 
     return value
 
-
-
 # testing
-text = input()
+text = input("input")
 print (Caesar_Encrypt (text))
